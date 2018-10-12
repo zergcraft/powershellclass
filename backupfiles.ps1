a90bb3a2cc56d9f387bd765952acabeb51221ff1
zergcraft <43459511+zergcraft@users.noreply.github.com>
2018-10-11 20:43:35 -0700

    Add files via upload

---
+++ backupfiles.ps1
@@ -0,0 +1,15 @@
+﻿#Raphael Vecina
+#This is a comment for my Script
+<#---------------------------------------------
+This is a block comment
+---------------------------------------------#>
+
+write-host "Saving the following files" `
+            "to the ""Backup"" folder:"
+
+get-childitem | where {
+    $_.LastWriteTime -ge "2011-01-01" } |
+foreach-object {
+    write-host " Backup\$($_.Name)"
+    copy-item $_ Backup\}
+
