a5de95be47dface6e7e245da7ff583aa5b483a26
zergcraft <43459511+zergcraft@users.noreply.github.com>
2018-10-11 20:58:57 -0700

    Add files via upload

--- backupfiles.ps1
+++ backupfiles.ps1
@@ -7,9 +7,16 @@ This is a block comment
 write-host "Saving the following files" `
             "to the ""Backup"" folder:"
 
+$BUCount = 0
+$BUSize = 0
+
+
 get-childitem | where {
     $_.LastWriteTime -ge "2011-01-01" } |
 foreach-object {
     write-host " Backup\$($_.Name)"
+    $BUSize += $_.Length
+    $BUCount ++
     copy-item $_ Backup\}
-
+write-host "Saved:" $BUCount "files," `
+                    $BUSize "bytes"
