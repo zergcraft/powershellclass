776d5f199fdb6980f9639c10b476ea93945024c8
zergcraft <43459511+zergcraft@users.noreply.github.com>
2018-11-09 10:33:13 -0800

    Add files via upload

--- get-averagefilesize.ps1
+++ get-averagefilesize.ps1
@@ -1,28 +1,32 @@
-﻿Function get-averagefilesize {
+﻿Function get-averagefilesize ([object]$Folder=$Foldermain) {
 #if no folder name is given then the function should default to looking in the PWD
 [IO.Directory]::SetCurrentDirectory((Get-Location))
 
-$Folder = $args[0]
+#if no folder name is given then the function defaults to current folder:
+if ($Folder -eq $null)
+{
+$Folder=(get-location).Path
 
+}
 
 #if given folder does not exist, return with an average size of zero
 if ( -not (test-path $Folder -pathtype container) )
 {
         write-host -foregroundcolor red "`a`n`"$Folder`" does not exist or is not a folder"
         write-host -foregroundcolor red "The average file size is 0 KBytes"
+        return
+        
 }
 
-if ($args[0] -eq $null)
-{
-    write-host "The average file size in the current folder is:0KBytes"
-    exit 1
-}
 
 
 
 #returns average file size for all the files in the folder
-$Average= "{0:N2}" -f ((Get-ChildItem "$Folder" | ?{$_ -is [IO.FileInfo]} | measure length -average).average/1KB )
-write-host "The average file size in the ""$Folder"" folder is:"$Average"KBytes"
+$Average= (Get-ChildItem $Folder  -Force | Measure-Object -Property Length -Sum).Sum 2> $null
+$AverageComplete = "{0:N0}" -f ($Average / 1KB) + "KB"
+write-host "The average file size in the ""$Folder"" folder is: "$AverageComplete""
 
 }
+
+$Foldermain=$args[0]
 get-averagefilesize
