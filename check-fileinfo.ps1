fc23a909cbb6fb4494e665e8515eef35fc49ee38
zergcraft <43459511+zergcraft@users.noreply.github.com>
2018-11-08 20:58:46 -0800

    Add files via upload

---
+++ check-fileinfo.ps1
@@ -0,0 +1,14 @@
+﻿$LenPos = 25 
+$DatePos = 36
+$LenLength = $DatePos - $lenPos
+get-content FileInfo.txt |
+foreach {
+    $Name = $_.SubString(0, $LenPos).Trim()
+    $Len = $_.SubString($LenPos, $LenLength).Trim()
+    $Date = $_.SubString($DatePos).Trim()
+    $File = get-item $Name
+    if ($File.Length -ne $Len)
+    {write-host "$Name length has changed"}
+    if ($File.LastWriteTime -ne $Date)
+    {write-host "$Name date has changed"}
+}
