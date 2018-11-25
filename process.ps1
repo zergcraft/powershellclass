8180d69e75f4df1f3a8e51ba04937bb46d5079d3
zergcraft <43459511+zergcraft@users.noreply.github.com>
2018-11-25 10:05:33 -0800

    Add files via upload

--- process.ps1
+++ process.ps1
@@ -1,21 +1,21 @@
 ﻿#shows a list of processes that show ID,NAME,WS,PrivateMemorySize and VirtualMemorySize for each process
 #last three should be in MB /1MB
 #Show processes in order of increasing WS size
-
-
-#should:
+#script should:
 #-use write-host cmdlet in conjunction with -f
 #-Use the same output formatting string to display the headings and values for each process
 #-Determine the name of the longest process and use that in your output formatting string so
 # that no more space is used than is necessary to show the process names
-#$LongestWS = (get-process | foreach {$_.WorkingSet} | Sort length  | select -last 1).Length
-#$LongestP = (get-process | foreach {$_.PrivateMemorySize} | Sort length  | select -last 1).Length
-#$LongestV = (get-process | foreach {$_.VirtualMemorySize} | Sort length  | select -last 1).Length
-#$formatstr = "{0,$LongestID}{1,$LongestName};{2,$LongestWS};{3,$LongestP};{4,$LongestV}" 
-#$LongestID = (get-process | foreach {$_.ID} | Sort length | select -last 1).Length
 
+#Get the longest name length on the list and use as the "max" padded value for the format str
 $LongestName = (get-process | foreach {$_.Name} | Sort length | select -last 1).Length
-$formatstr = "{0} {1,$LongestName} {2} {3} {4}" 
+
+#assign format string value; format pad each value by 5 characters at the end by using negative(-5) 
+$formatstr = "{0,-5} {1,-$LongestName} {2,-5} {3,-5} {4,-5}"
+
+#write the headers using the format string
 write-host ("$formatstr" -f "ID","Name","WS","Private","Virtual")
-write-host ("$formatstr" -f "-","-","-","-","-")
-Get-Process | sort WS| ForEach-Object {write-host ("$formatstr" -f $_.ID,$_.Name,$_.WorkingSet,$_.PrivateMemorySize,$_.VirtualMemorySize)}
+write-host ("$formatstr" -f "----","-----------","---","-----","-----")
+
+#Get each process, sort by WS, and for each process, write each process' ID,Name,WorkingSet,VM,PM. The last 3 are divided by 1MB and rounded by 1 decimal point
+Get-Process | sort WS| ForEach-Object {write-host ("$formatstr" -f $_.ID,$_.Name,[math]::Round($_.WorkingSet/1MB,1),[math]::Round($_.PrivateMemorySize/1MB,1),[math]::Round($_.VirtualMemorySize/1MB,1))}
