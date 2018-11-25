6354416759ce66fc785b687a49a47c3aab434b7f
zergcraft <43459511+zergcraft@users.noreply.github.com>
2018-11-25 10:16:38 -0800

    Add files via upload

--- history.ps1
+++ history.ps1
@@ -1,13 +1,10 @@
 ﻿#PS commands to show
 #-which command in the current session took the longest amount of time to execute
 #-hint: you can add a duration property to the objects - this will allow you to sort them by their duration
-$duration=0
-$history=0
 
-$history = get-history 
-$duration = ($history | foreach-object {
-add-member -inputobject $_ noteproperty duration ($_.EndExecutiontime - $_.StartExecutionTime )-force;$_} ) 
+#$duration=0
+#$history=0
+#$history = get-history 
+$duration = (get-history | foreach-object {
+add-member -inputobject $_ noteproperty duration (($_.EndExecutiontime - $_.StartExecutionTime).totalmilliseconds)-force;$_} ) 
 $duration | sort duration -desc | select -first 1 | format-table id,duration,commandline 
-
-
- 
