f6f86ccc9effe8ee150234d344cc7c05f4e6205b
zergcraft <43459511+zergcraft@users.noreply.github.com>
2018-11-29 20:59:21 -0800

    Add files via upload

---
+++ sum.ps1
@@ -0,0 +1,19 @@
+﻿#adds all numbers in command line parameter then displays sum
+
+$ErrorActionPreference="SilentlyContinue"
+try
+{
+$total=0
+foreach ($arg in $args)
+{
+    
+    $Total+=[double]$arg
+}
+}
+catch
+{
+write-host "The statement produced an error, ignoring and continuing to add"
+
+}
+write-host "The total is:" $total
+$ErrorActionPreference="Continue"
